dba6c243e4387cc9f30be22f4b98981a239640cc
Giselle Koo <gisellegk@gmail.com>
2017-04-02 09:26:35 -0400

    no canny

--- candlefinder.cpp
+++ candlefinder.cpp
@@ -63,14 +63,37 @@ int main(int argc, char* argv[])
     if(keypoints.size() > 0) {
       std::cout << "(" << keypoints[0].pt.x << ", " << keypoints[0].pt.y << ")" << std::endl;
     }
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours( frame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    /// Approximate contours to polygons + get bounding rects and circles
+    vector<vector<Point> > contours_poly( contours.size() );
+    vector<Rect> boundRect( contours.size() );
+    vector<Point2f>center( contours.size() );
+    vector<float>radius( contours.size() );
 
-    Canny( frame, frame, thresh, thresh*3, 3 );
+    for( int i = 0; i < contours.size(); i++ )
+    { approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+      boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+      minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+    }
+    /// Draw polygonal contour + bonding rects + circles
+    Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
+    for( int i = 0; i< contours.size(); i++ )
+    {
+      Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+      drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+      rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+      circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+    }
+
+    /*Canny( frame, frame, thresh, thresh*3, 3 );
     Mat dst;
     dst.create(frame.size(), frame.type());
     dst = Scalar::all(0);
-    frame.copyTo(dst, frame);
+    frame.copyTo(dst, frame);*/
 
-    imshow("robovision", frame ); // show frame on computer output window
+    imshow("robovision", drawing ); // show frame on computer output window
 
     /* Escape = kill program */
     int keypress = waitKey(10)%255;
